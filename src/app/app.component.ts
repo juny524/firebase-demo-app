579449b7c4ec5f2fbffe81004a354f3afb9b482d
juny524 <juny5241@gmail.com>
2022-11-03 13:36:08 +0900

    とりあえずデータを読み出すものを作りました。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { AuthService } from 'FirebaseStorage';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,6 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent {
   title = 'demo-app';
-  user$ = this.auth.user$;
 
   constructor(private auth: AuthService) {}
   login() {
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,35 @@
 import { Injectable } from '@angular/core';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { auth } from 'firebase';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore,collection, getDocs } from "firebase/firestore";
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  user$ = this.afAuth.user;
-  constructor(private afAuth: provideFirebaseApp) { }
+
+
+
+  constructor() {  }
+  async getStore(){
+    const app = initializeApp(environment.firebase);
+    const auth = getAuth(app);
+    const db = getFirestore(app);
+    const querySnapshot = await getDocs(collection(db, "testid"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+
+    
+
+  }
   login() {
-    this.afAuth
-      .signInWithPopup(new auth.GoogleAuthProvider())
-      .then((result) => {
-        console.log(result);
-      });
+
+    
+
   }
   logout() {
-    this.afAuth.signOut();
+
   }
 }
